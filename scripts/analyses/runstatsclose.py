9fe4616b7332c464a9f7165d80767311b8dc22a2
Tom Zhang <tom.zhang989@gmail.com>
2023-05-30 19:22:37 -0400

    removes unused code and comments

--- scripts/analyses/runstatsclose.py
+++ scripts/analyses/runstatsclose.py
@@ -17,7 +17,6 @@ closedata = pd.read_csv("first3close.csv")
 # TODO: Create CloseData that's all of the required tables put together. 
 
 
-# testvalue = closedata.at["2018-01-02", "AOSclose"]
 
 # calcAdvDec calculates Advance and decline for inx (a data frameindex) and ticksym (stock ticker symbol)
 # returns if the stock advanced, declined or remained the same that day
@@ -33,15 +32,9 @@ def calcAdvDec (inx):
     return sumadvdec
 
 
-# returns the series of cumulative advance and declines
-def allAdvDec(x):
-    return sum 
-
 closedata = closedata.set_index("Date")
 testvalues = closedata.index
 
-
-# closedata["AdvDec"] = list(map(calcAdvDec, testvalues))
 
 i = 0
 anslist = []
@@ -64,15 +57,10 @@ dates = closedata.index
 fig, (ax0) = plt.subplots(1, 1, sharex=True, constrained_layout=True)
 # # Price:
 ax0.plot(dates, closedata["sumAdvDec"] , label='culmadvdec')
+ax0.plot(dates, closedata["AdvDec"] , label='instadvdec')
 ax0.legend(loc='upper right')
 ax0.set_ylabel('Culm advdec') 
 ax0.set_title('MMM AOS ABT stock price', loc='left')
 
 
 print(closedata)
-# The Formula for Advance/Decline (A/D) Line Is:
-# A/D = Net Advances  +  { PA, if PA value exists else 0
-# where:
-# Net Advances == Difference between number of daily ascending and declining stocks 
-#  PA == Previous Advances == Prior indicator reading
-# ​
