5ab222ac5d1379e12355fe8d0df7697aa979616d
Tom Zhang <tom.zhang989@gmail.com>
2023-06-09 10:34:55 -0400

    adds tryexcept handler for failed data dl

--- scripts/imports/importclose.py
+++ scripts/imports/importclose.py
@@ -19,11 +19,18 @@ close= initial[[symlst[0]  + 'close', symlst[0]  + 'open']]
 
 # Makes merged table of close
 # data['AdvDec'] = 1 if (data.High - data.Low) > 0 else -1
+
+failedLst = []
 for sym in symlst[1:]: 
-  data = yf.download(sym, start_date, end_date)
-  data[sym + 'close'] = data.Close
-  data[sym + 'open'] = data.Open
-  close = pd.merge(close, data[[sym + 'close', sym + 'open']], how='inner', on="Date")
+  try: 
+    data = yf.download(sym, start_date, end_date)
+    data[sym + 'close'] = data.Close
+    data[sym + 'open'] = data.Open
+    close = pd.merge(close, data[[sym + 'close', sym + 'open']], how='inner', on="Date")
+  except:
+    failedLst = failedLst + [sym]
+    continue
 
-
+missingdata = pd.DataFrame(data={'failedDL':failedLst})
+missingdata.to_csv("failedDLclose.csv")
 close.to_csv("500close.csv")
