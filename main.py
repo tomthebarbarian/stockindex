9ae287aa68df4ba76546eeb808f53740b2920c1e
Tom Zhang <tom.zhang989@gmail.com>
2023-06-19 22:58:23 -0400

    creates some webpage templates for a flask example

--- main.py
+++ main.py
@@ -7,15 +7,27 @@
 # TODO Create visualisations with matplot lib
 
 # TODO Host the results on a window. 
-import sys
-from flask import Flask
-for p in sys.path:
-    print( p )
+from flask import Flask, render_template
+# import sys,os
+
+# script_dir = os.path.dirname( __file__ )
+# mymodule_dir = os.path.join( script_dir, '..', '..', 'data')
+# sys.path.append( mymodule_dir )
 
 app = Flask(__name__)
 
+# I guess this is the "html string"
+@app.route("/")
 def home():
-    return "some text on teh page"
+    return render_template("home.html")
+
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
