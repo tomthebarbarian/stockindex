15f2ffd10ba0233878def0c63fb569932974e92e
Tom Zhang <tom.zhang989@gmail.com>
2023-06-29 21:47:45 -0400

    adds the comment for the breadth analysis

---
+++ scripts/analyses/breadth.py
@@ -0,0 +1,7 @@
+
+# For each of the 11 sp 500 sectors
+# Calculate the percentage of stocks per sector that fullfil the following:
+# Have a higher closing price than the respective 20 day simple moving average
+
+# Sum the percentages for the 11 sectors for a score out of 1100
+# Colour it bullish if score is <200, and bearish if >1000
