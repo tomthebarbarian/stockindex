5f728736c003c0bbe13c6983510fe9c3e1ad4587
Tom Zhang <tom.zhang989@gmail.com>
2023-08-16 21:57:12 -0400

    restructures project like flask tutorial

---
+++ flaskr/main.py
@@ -0,0 +1,60 @@
+# TODO Pull data from finance using yfinance
+
+# TODO Load into workspace dataframe
+
+# TODO Create analyses with numpy etc
+
+# TODO Create visualisations with matplot lib
+
+# TODO Host the results on a window. 
+from flask import Flask, render_template
+import sys,os
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_datepicker import datepicker
+
+script_dir = os.path.dirname( __file__ )
+SCRIPT_FOLDER = os.path.join(script_dir,'scripts','analyses')
+sys.path.append( SCRIPT_FOLDER )
+
+mymodule_dir = os.path.join( script_dir,'data')
+sys.path.append( mymodule_dir )
+
+app = Flask(__name__)
+IMG_FOLDER = os.path.join('static', 'IMG')
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
+Bootstrap(app)
+datepicker(app)
+
+
+import breadth
+
+def picker(id=".datepicker", # identifier will be passed to Jquery to select element
+           dateFormat='yy-mm-dd', # can't be explained more !
+           maxDate='2023-08-30', # maximum date to select from. Make sure to follow the same format yy-mm-dd
+           minDate='2017-12-01', # minimum date
+           btnsId='.btnId' # id assigned to instigating buttons if needed
+    ):
+    return 
+
+# I guess this is the "html string"
+@app.route("/")
+def home():
+
+    return render_template("home.html", currbreadth = breadth.generateBreadth())
+
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/graph")
+def graph():
+    AdvDecPlot = os.path.join(app.config['UPLOAD_FOLDER'], 'advdecplt2019.png')
+    return render_template("graph.html",user_image=AdvDecPlot)
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
---
+++ flaskr/scripts/analyses/breadth.py
@@ -0,0 +1,87 @@
+# TODO Create analyses with numpy etc
+import sys,os
+# from .scripts.imports import spinx
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
+from sqlalchemy import create_engine
+import psycopg2
+
+script_dir = os.path.dirname( __file__ )
+mymodule_dir = os.path.join( script_dir, '..',  '..', 'data')
+sys.path.append( mymodule_dir )
+mymodule_dir = os.path.join( script_dir, '..', '..', 'results')
+sys.path.append( mymodule_dir )
+
+# print("does", 'SPTickerWiki.csv', "exist?", os.path.exists('SPTickerWiki.csv'))
+# print("does", 'checkinxlst.py', "exist?", os.path.exists('checkinxlst.py'))
+# print(script_dir, '..',  '..', 'data')
+
+from datetime import datetime, timedelta
+
+
+# TODO:
+# For each of the 11 sp 500 sectors
+# Calculate the percentage of stocks per sector that fullfil the following:
+# Have a higher closing price than the respective 20 day simple moving average
+
+# Sum the percentages for the 11 sectors for a score out of 1100
+# Colour it bullish if score is <200, and bearish if >1000
+
+# TODO: Should probably make a bargraph comparing the percentages and donut chart of the biggest contributors to low/high scores
+# print("this is sys path", sys.path)
+
+spdata = pd.read_csv(os.path.join(script_dir, '..',  '..', 'data','SPTickerWiki.csv'))
+# spdata = pd.read_csv("data/SPTickerWiki.csv")
+# spdata = pd.read_csv("SPTickerWiki.csv")
+columnnames = spdata.columns
+# print(columnnames)
+GICSSectors = spdata['GICS Sector'].unique()
+
+databysector = {}    
+# closeData = pd.read_csv("results/500close.csv")
+
+
+alchemyEngine   = create_engine('postgresql+psycopg2://test:@127.0.0.1:5432/stockindex', pool_recycle=3600)
+dbConnection    = alchemyEngine.connect()
+closeData       = pd.read_sql("select * from \"spclose\"", dbConnection)
+pd.set_option('display.expand_frame_repr', False)
+
+# closeData = closeData.set_index("Date")
+
+# Date Format YYYY-MM-DD
+today = datetime.today()
+# Should do nearest weekday
+# end_date = (today - timedelta(days=1)).date()
+# end_date = (today - timedelta(days=1)).strftime('%Y-%m-%d').date()
+end_date = datetime.strptime('2023-08-04', '%Y-%m-%d').date()
+
+def generateBreadth():
+  # 
+  sumPercAdvDec = 0
+  advDecPerSector = []
+  # Create lists by sector
+  for sector in GICSSectors:
+    databysector[sector] = spdata[(spdata['GICS Sector'] == sector)]['Symbol']
+  # Calc moving average and if close price is higher for the data add 1 to sector advdec
+  # Select by column heads
+    sectorAdvDec = 0
+    # print(databysector[sector])
+    for sym in databysector[sector]: 
+      colname = sym + "close"
+      valatloc = closeData[(closeData["Date"] == end_date)][colname].values[0]
+      if np.mean(closeData[colname]) < valatloc:
+        sectorAdvDec += 1
+      # print(sectorAdvDec, sym)
+    if sectorAdvDec < 0:
+      sectorAdvDec = 0
+    else:
+      sectorAdvDec = (sectorAdvDec*100)/databysector[sector].size
+    # print(sectorAdvDec, sector)
+    sumPercAdvDec += sectorAdvDec
+    advDecPerSector += [sector, sectorAdvDec]
+
+  # print(sumPercAdvDec, 'inside module')
+  return sumPercAdvDec
+
